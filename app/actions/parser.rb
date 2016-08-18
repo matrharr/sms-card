95761abf9b48783909365a0d7bf32b548bcbc2b1
Matt Harris <matrharr@gmail.com>
2016-08-17 23:09:54 -0700

    add parser

---
+++ app/actions/parser.rb
@@ -0,0 +1,31 @@
+class Parser
+
+  # including package
+
+  include ShareContact
+
+  def parse_message(text, user)
+
+    if text[0].downcase == 'h'
+
+      help_menu = "cat: concatenate files\n
+            pwd: print working directory\n
+            cd: change the working directory\n
+            cp: copy files\nenv: set environment\n
+            grep: search for pattern\n
+            ls: list contents\n"
+
+      return [user, help_menu]
+
+    else
+
+      # include ability to detect which package
+
+      sc_arr = bundle_info(text)
+
+      return sc_arr
+    end 
+    
+  end
+
+end
