ee8fd306917f9445fd57677ed0794d6584b61b86
Matt Harris <matrharr@gmail.com>
2016-08-17 23:02:25 -0700

    create broadcast service

---
+++ app/services/broadcast_service.rb
@@ -0,0 +1,23 @@
+class BroadcastService
+
+  def create_client
+    account_sid = ENV['ACCOUNT_SID']
+    auth_token = ENV['AUTH_TOKEN']
+    @client = Twilio::REST::Client.new account_sid, auth_token
+  end
+
+  def send_message(payload_arr)
+    recipient = payload_arr[0]
+    message = payload_arr[1]
+
+    @client.messages.create(
+        from: '+19494415310',
+        to: recipient,
+        body: message
+        )
+  end
+
+
+
+
+end
