59f55fe6f9f8d0ac682cc60079c30341917ec8ac
Matt Harris <matrharr@gmail.com>
2016-08-18 22:36:23 -0700

    test enviroment vars

--- app/services/broadcast_service.rb
+++ app/services/broadcast_service.rb
@@ -3,7 +3,9 @@ module BroadcastService
   def create_client
     account_sid = ENV['ACCOUNT_SID']
     auth_token = ENV['AUTH_TOKEN']
-    @client = Twilio::REST::Client.new account_sid, auth_token
+    p account_sid
+    p auth_token
+    @client = Twilio::REST::Client.new(account_sid, auth_token)
   end
 
   def send_message(payload_arr)
