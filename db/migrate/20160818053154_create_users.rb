31964b5cb36a6799459cb44fb091bc90a5a9d7eb
Matt Harris <matrharr@gmail.com>
2016-08-18 19:10:13 -0700

    edit db

--- db/migrate/20160818053154_create_users.rb
+++ db/migrate/20160818053154_create_users.rb
@@ -1,7 +1,19 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-
+      t.string :first_name
+      t.string :last_name
+      t.string :phone_number
+      t.string :email
+      t.string :github
+      t.string :linkedin
+      t.string :portfolio
+      t.string :facebook
+      t.string :twitter
+      t.string :instagram
+      t.string :job_title
+      t.string :company
+      
       t.timestamps
     end
   end
