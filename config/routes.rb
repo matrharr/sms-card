e0f2945f7fc6f1745ad3450c9fe136c8228272d0
Matt Harris <matrharr@gmail.com>
2016-08-18 21:18:31 -0700

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resource :broadcast, only: :create
+  resources :broadcasts, only: [:index, :create]
+  root 'broadcasts#index'
 end
