c776edcb3ee142836e9493e4f5bfe8b54552726b
Matt Harris <matrharr@gmail.com>
2016-08-26 17:54:36 -0700

    delete spaces in sent msg

--- app/packages/share_contact.rb
+++ app/packages/share_contact.rb
@@ -35,14 +35,7 @@ module ShareContact
     
     user = User.find_by(phone_number: sender)
   
-    message_sent = "#{user.first_name} has sent you an SMSCard:\n
-                    #{user.first_name} #{user.last_name}\n
-                    #{user.job_title} at #{user.company}\n
-                    #{user.phone_number}\n
-                    #{user.email}\n
-                    gh: #{user.github}\n
-                    in: #{user.linkedin}\n
-                    portfolio: #{user.portfolio}"
+    message_sent = "#{user.first_name} has sent you an SMSCard:\n#{user.first_name} #{user.last_name}\n#{user.job_title} at #{user.company}\n#{user.phone_number}\n#{user.email}\ngh: #{user.github}\nin: #{user.linkedin}\nportfolio: #{user.portfolio}"
 
 
     p recipient
