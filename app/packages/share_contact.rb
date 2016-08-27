4c42b8480207184131eb025cbadc0043c9d6ff4e
Matt Harris <matrharr@gmail.com>
2016-08-27 10:22:54 -0700

    Add contact info sent to sender
    Changed output of share contact package to a 2D array with smscard msg for recipient and recipient name and phone # for sender. Had to add some logic to get recipient name. Should consider the possibility of separating each messages' logic into separate files.

--- app/packages/share_contact.rb
+++ app/packages/share_contact.rb
@@ -1,6 +1,6 @@
 module ShareContact
 
-  def bundle_info(message_received, sender)
+  def bundle_info(message_received, sender_number)
 
     # input: "Sc n matt harris; p +16154064891; soc"
 
@@ -21,25 +21,30 @@ module ShareContact
     # ]
 
     # make more flexible
-    recipient = ''
+    recipient_number = ''
     msg.map! do |line|
       if line[0].downcase == 'p'
         p "test"
         line = line.split('')
-        recipient = line[2..-1].join
-        p recipient
+        recipient_number = line[2..-1].join
       end
     end
 
+    recipient_name = ''
+    first_line = msg[0]
+    recipient_name = first_line[5..-1]
+
     # generalize
     
-    user = User.find_by(phone_number: sender)
+    user = User.find_by(phone_number: sender_number)
   
-    message_sent = "#{user.first_name} has sent you an SMSCard:\n#{user.first_name} #{user.last_name}\n#{user.job_title} at #{user.company}\n#{user.phone_number}\n#{user.email}\ngh: #{user.github}\nin: #{user.linkedin}\nportfolio: #{user.portfolio}"
+    recipient_message = "#{user.first_name} has sent you an SMSCard:\n#{user.first_name} #{user.last_name}\n#{user.job_title} at #{user.company}\n#{user.phone_number}\n#{user.email}\ngh: #{user.github}\nin: #{user.linkedin}\nportfolio: #{user.portfolio}"
 
+    # grab recipient_name from msg_received, save to var
+    sender_message = "New Contact from SMSCard:\n#{recipient_name}\n#{recipient_number}"
 
-    p recipient
-    payload = [recipient, message_sent]
+    
+    payload = [[recipient_number, recipient_message], [sender_number, sender_message]]
 
     return payload
 
